ca9cec8c5fa7a225653320690720557aae5ff161
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2016-11-12 22:30:51 +0100

    jpg extension

--- src/server/images.js
+++ src/server/images.js
@@ -2,7 +2,7 @@ import glob from 'glob';
 import path from 'path';
 
 const IMAGES_FOLDER = path.join(__dirname, '../../public/images');
-const IMAGES_EXT = '.png';
+const IMAGES_EXT = '.JPG';
 const IMAGES_GLOB = `${ IMAGES_FOLDER }/*${ IMAGES_EXT }`;
 
 const PUBLIC_URL = '/public/images';
